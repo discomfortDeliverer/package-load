2bc376e0306bb1651518dba858930bf94b331685
Maksim <maxorient1999@gmail.com>
2024-09-23 15:47:39 +0300

    refactor: В классе Main удалил неиспользуемые импорты

--- src/main/java/ru/discomfortdeliverer/Main.java
+++ src/main/java/ru/discomfortdeliverer/Main.java
@@ -1,23 +1,12 @@
 package ru.discomfortdeliverer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
 import ru.discomfortdeliverer.console.ConsoleMenu;
-import ru.discomfortdeliverer.exception.InvalidFilePathException;
-import ru.discomfortdeliverer.exception.InvalidInputException;
 import ru.discomfortdeliverer.parcel.FileParcelLoader;
-import ru.discomfortdeliverer.parcel.Parcel;
 import ru.discomfortdeliverer.parcel.ParcelInputValidator;
 import ru.discomfortdeliverer.truck.FileTruckLoader;
-import ru.discomfortdeliverer.truck.Truck;
 import ru.discomfortdeliverer.truck.TruckLoadManager;
 import ru.discomfortdeliverer.truck.TruckUtils;
-import ru.discomfortdeliverer.view.ConsoleTruckView;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 @Slf4j
