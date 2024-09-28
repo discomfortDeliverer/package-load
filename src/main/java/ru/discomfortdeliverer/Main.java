f9906686596ef0d172f52bc321bf634308c5a227
Maksim <maxorient1999@gmail.com>
2024-09-28 14:37:52 +0300

    feat: Добавил Spring Boot и Spring shell

--- src/main/java/ru/discomfortdeliverer/Main.java
+++ src/main/java/ru/discomfortdeliverer/Main.java
@@ -1,6 +1,8 @@
 package ru.discomfortdeliverer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.discomfortdeliverer.console.ConsoleMenu;
 import ru.discomfortdeliverer.parcel.FileParcelLoader;
 import ru.discomfortdeliverer.parcel.ParcelInputValidator;
@@ -10,15 +12,17 @@ import ru.discomfortdeliverer.truck.TruckUtils;
 import java.util.Scanner;
 
 @Slf4j
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        FileParcelLoader fileParcelLoader = new FileParcelLoader(new ParcelInputValidator());
-        TruckLoadManager truckLoadManager = new TruckLoadManager();
-        FileTruckLoader fileTruckLoader = new FileTruckLoader();
-        TruckUtils truckUtils = new TruckUtils();
-        ConsoleMenu consoleMenu = new ConsoleMenu(scanner, fileParcelLoader, truckLoadManager
-        ,fileTruckLoader, truckUtils);
-        consoleMenu.startConsoleMenu();
+        SpringApplication.run(Main.class, args);
+//        Scanner scanner = new Scanner(System.in);
+//        FileParcelLoader fileParcelLoader = new FileParcelLoader(new ParcelInputValidator());
+//        TruckLoadManager truckLoadManager = new TruckLoadManager();
+//        FileTruckLoader fileTruckLoader = new FileTruckLoader();
+//        TruckUtils truckUtils = new TruckUtils();
+//        ConsoleMenu consoleMenu = new ConsoleMenu(scanner, fileParcelLoader, truckLoadManager
+//        ,fileTruckLoader, truckUtils);
+//        consoleMenu.startConsoleMenu();
     }
 }
